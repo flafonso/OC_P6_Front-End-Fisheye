f1710f53fbf5cefa5e047682d0eacdb3aa526b49
Flavien Afonso <af.flavien@gmail.com>
2023-11-03 06:34:54 +0100

    move dom management from the photographer page to templates

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,48 +1,7 @@
-function photographModal(photographer) {
-  const titleEl = document.querySelector(".modal header h2");
-  titleEl.innerHTML = `Contactez-moi
-                      <br>
-                      ${photographer.name}`;
-}
-
-function photographLikePrice(photographer) {
-  const priceEl = document.querySelector(".price");
-  priceEl.textContent = `${photographer.price} / jour`;
-
-  const totalLikeEl = document.querySelector(".total-like");
-  const totalLikeValue = document.createElement("span");
-  totalLikeValue.textContent = "12 043";  //Until we get the real value from the data
-  totalLikeEl.prepend(totalLikeValue);
-}
-
-function photographHeader(photographer) {
-  const photographHeader = document.querySelector(".photograph-header");
-  const picture = `assets/photographers/profile_picture/${photographer.portrait}`;
-
-  const textArea = document.createElement("div");
-  textArea.classList.add("text-area");
-
-  const nameEl = document.createElement("h1");
-  nameEl.textContent = photographer.name;
-
-  const locationEl = document.createElement("p");
-  locationEl.classList.add("location");
-  locationEl.textContent = `${photographer.city}, ${photographer.country}`;
-
-  const taglineEl = document.createElement("p");
-  taglineEl.textContent = photographer.tagline;
-
-  const imgEl = document.createElement("img");
-  imgEl.setAttribute("src", picture);
-
-  textArea.append(nameEl, locationEl, taglineEl);
-  photographHeader.append(textArea, imgEl);
-}
-
 async function displayData(photographer) {
-  photographHeader(photographer);
-  photographLikePrice(photographer);
-  photographModal(photographer);
+  photographHeader(photographer);  // eslint-disable-line no-undef
+  photographLikePrice(photographer);  // eslint-disable-line no-undef
+  photographModal(photographer);  // eslint-disable-line no-undef
 }
 
 async function init() {
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -32,3 +32,45 @@ function photographerTemplate(data) {  // eslint-disable-line no-unused-vars
   }
   return { name, picture, id, city, country, tagline, price, getUserCardDOM };
 }
+
+
+function photographModal(photographer) {  // eslint-disable-line no-unused-vars
+  const titleEl = document.querySelector(".modal header h2");
+  titleEl.innerHTML = `Contactez-moi
+                      <br>
+                      ${photographer.name}`;
+}
+
+function photographLikePrice(photographer) {  // eslint-disable-line no-unused-vars
+  const priceEl = document.querySelector(".price");
+  priceEl.textContent = `${photographer.price} / jour`;
+
+  const totalLikeEl = document.querySelector(".total-like");
+  const totalLikeValue = document.createElement("span");
+  totalLikeValue.textContent = "12 043";  //Until we get the real value from the data
+  totalLikeEl.prepend(totalLikeValue);
+}
+
+function photographHeader(photographer) {  // eslint-disable-line no-unused-vars
+  const photographHeader = document.querySelector(".photograph-header");
+  const picture = `assets/photographers/profile_picture/${photographer.portrait}`;
+
+  const textArea = document.createElement("div");
+  textArea.classList.add("text-area");
+
+  const nameEl = document.createElement("h1");
+  nameEl.textContent = photographer.name;
+
+  const locationEl = document.createElement("p");
+  locationEl.classList.add("location");
+  locationEl.textContent = `${photographer.city}, ${photographer.country}`;
+
+  const taglineEl = document.createElement("p");
+  taglineEl.textContent = photographer.tagline;
+
+  const imgEl = document.createElement("img");
+  imgEl.setAttribute("src", picture);
+
+  textArea.append(nameEl, locationEl, taglineEl);
+  photographHeader.append(textArea, imgEl);
+}
