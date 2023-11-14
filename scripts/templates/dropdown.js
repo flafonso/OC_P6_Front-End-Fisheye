f362d2bb41d94f56cf1616dcaf2eb669409d312e
Flavien Afonso <af.flavien@gmail.com>
2023-11-14 13:45:03 +0100

    added the moving arrow in the drop-down menu

--- scripts/templates/dropdown.js
+++ scripts/templates/dropdown.js
@@ -6,33 +6,41 @@ const arrowEl = document.querySelector(".arrow");
 const optionsMenuEl = document.querySelector(".options-menu");
 const optionsEl = document.querySelectorAll(".options-menu p");
 
-// console.log("selectEl", selectEl);
-// console.log("selectedEl", selectedEl);
-console.log("arrowEl", arrowEl);
-// console.log("optionsMenuEl", optionsMenuEl);
-
 selectEl.addEventListener("click", () => {
+  // Little change of dropdown style when it's open
   dropdownEl.classList.toggle("dropdown--open");
+  // Show or hide options menu
   optionsMenuEl.classList.toggle("options-menu--visible");
+  arrowEl.classList.toggle("arrow--rotate");
 });
 
+// Click event handler for options
 optionsEl.forEach((option) => {
   option.addEventListener("click", () => {
-    // console.log("option", option);
     selectedEl.innerHTML = option.innerHTML;
     optionsMenuEl.classList.remove("options-menu--visible");
     dropdownEl.classList.remove("dropdown--open");
+    arrowEl.classList.remove("arrow--rotate");
+
+    // Re-displays the option that is no longer selected and removes the separator bars
     optionsEl.forEach((option) => {
-      if (option.classList.contains("active")) {
-        option.classList.remove("active");
+      if (option.classList.contains("hide-option")) {
+        option.classList.remove("hide-option");
       }
       if (option.classList.contains("middle-separation")) {
         option.classList.remove("middle-separation");
       }
     });
+
+    // Hide the option you have just clicked from the list of options
+    option.classList.add("hide-option");
+    /**
+     * By default, Date (optionsEl[1]) has the separators because it
+     * is in the middle of the list at the start, but if Popularity
+     * (optionsEl[0]) is not selected, it ends up in the middle, so it takes the separators.
+     */
     optionsEl[0] === option
       ? optionsEl[1].classList.add("middle-separation")
       : optionsEl[0].classList.add("middle-separation");
-    option.classList.add("active");
   });
 });
